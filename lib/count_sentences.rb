c0f5fc82027e3f966cd038f480262b8d827fb2e8
vicki29644 <vicki29644@icloud.com>
2020-05-04 23:17:34 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")
   end
 
-  def question?
 
+  def question?
+  self.end_with?("?")
   end
 
   def exclamation?
-
+  self.end_with?("!")
   end
 
-  def count_sentences
-
-  end
+   def count_sentences 
+self.split(/[$.|?|!]+/).count 
+end
+# monkey patching an existing method
+def upcase
+puts ' not up casing for you anymore!'
+ end
 end
